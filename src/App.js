aefba8aa6c534c6b9cad4222796bb5264ba26fc5
Lucas Silveira <lucassilveira@dcc.ufmg.br>
2018-09-22 15:08:14 -0300

    Implements burger components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-           <p> Testing </p>
+           <BurgerBuilder />
         </Layout>
       </div>
     );
